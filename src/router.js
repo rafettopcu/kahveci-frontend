c96cf2188f8bea18f6e803592d86789b826db004
Rafet Topçu <rafet1998@gmail.com>
2020-03-22 18:41:15 +0300

    orders added

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import VueJwtDecode from "vue-jwt-decode";
+
 import Home from "./views/Home";
 import Register from "./views/Register";
 import Login from "./views/Login";
@@ -7,18 +9,19 @@ import Stores from "./views/Stores";
 import Products from "./views/Products";
 import Wallet from "./views/Wallet";
 import Cart from "./views/Cart";
-import VueJwtDecode from "vue-jwt-decode";
+import Orders from "./views/Orders";
+import Dashboard from "./views/admin/Dashboard";
 
 Vue.use(VueRouter);
 
 const ifAuthenticated = (to, from, next) => {
   const token = Vue.ls.get("token");
 
-  const authUser = Vue.ls.get("token");
-
   if (to.meta.requiresAuth) {
+    const authUser = Vue.ls.get("token");
     if (authUser) {
       const { role } = VueJwtDecode.decode(token);
+      console.log(VueJwtDecode.decode(token));
 
       if (to.meta.roles && to.meta.roles.length > 0) {
         if (to.meta.roles.includes(role)) {
@@ -104,6 +107,25 @@ const routes = [
       requiresAuth: true
     },
     component: Wallet
+  },
+  {
+    path: "/orders",
+    name: "orders",
+    beforeEnter: ifAuthenticated,
+    meta: {
+      requiresAuth: true
+    },
+    component: Orders
+  },
+  {
+    path: "/admin",
+    name: "dashboard",
+    beforeEnter: ifAuthenticated,
+    meta: {
+      requiresAuth: true,
+      roles: [1]
+    },
+    component: Dashboard
   }
 ];
 
@@ -111,7 +133,7 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
-  scrollBehavior() {
+  scrollBehavior () {
     return { x: 0, y: 0 };
   }
 });
